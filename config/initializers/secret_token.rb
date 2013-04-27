e1942e232c5c5728b79c9ac0b902f24bd1516b16
charlierudolph <charles.rudolph@originate.com>
2013-04-27 12:11:53 -0700

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cyvasse::Application.config.secret_token = 'c05fb7af1160bfbcf6187bb9b18a525eacc269d79d61583634bbef677608d1e0a713a6b0042fe103d722c29412ca7d9562a97c076616765741459a30a4cf5466'
