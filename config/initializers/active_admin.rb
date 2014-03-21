8a5e7eec0630f087fea37ef2b0f17cca82a6df0d
charlierudolph <charles.rudolph@originate.com>
2014-03-21 09:25:47 -0700

    Fix admin logout link

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -6,6 +6,7 @@ ActiveAdmin.setup do |config|
   config.current_user_method = :current_user
   config.filters = false
   config.logout_link_path = :destroy_user_session_path
+  config.logout_link_method = :delete
   config.root_to = 'users#index'
   config.site_title = "Cyvasse"
 end
