ff77d5376415c9c64b4ab5bd6050f29d3de46dde
Charles Rudolph <charles.w.rudolph@gmail.com>
2021-01-08 07:47:31 -0800

    fix frontend tests

--- frontend/src/app/components/yaniv/yaniv-game-scoreboard-dialog/yaniv-game-scoreboard-dialog.component.spec.ts
+++ frontend/src/app/components/yaniv/yaniv-game-scoreboard-dialog/yaniv-game-scoreboard-dialog.component.spec.ts
@@ -1,6 +1,22 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { AppModule } from "src/app/app.module";
+import { GameState, IGame } from "../../../shared/dtos/yaniv/game";
 
 import { YanivGameScoreboardDialogComponent } from "./yaniv-game-scoreboard-dialog.component";
+
+function buildTestGame(): IGame {
+  return {
+    gameId: 1,
+    hostUserId: 1,
+    options: { playTo: 100 },
+    playerStates: [],
+    state: GameState.ROUND_ACTIVE,
+    roundScores: [],
+    actionToUserId: 1,
+    cardsOnTopOfDiscardPile: [],
+  };
+}
 
 describe("YanivGameScoreboardDialogComponent", () => {
   let component: YanivGameScoreboardDialogComponent;
@@ -8,7 +24,13 @@ describe("YanivGameScoreboardDialogComponent", () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [YanivGameScoreboardDialogComponent],
+      imports: [AppModule],
+      providers: [
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: { game: buildTestGame() },
+        },
+      ],
     }).compileComponents();
   });
 
--- frontend/src/app/components/yaniv/yaniv-game-show/yaniv-game-show.component.spec.ts
+++ frontend/src/app/components/yaniv/yaniv-game-show/yaniv-game-show.component.spec.ts
@@ -1,20 +1,6 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { AppModule } from "src/app/app.module";
-import { GameState, IGame, IPlayerState } from "src/app/shared/dtos/yaniv/game";
 import { YanivGameShowComponent } from "./yaniv-game-show.component";
-
-function buildTestGame(playerStates: IPlayerState[]): IGame {
-  return {
-    gameId: 1,
-    hostUserId: 1,
-    options: { playTo: 100 },
-    playerStates,
-    state: GameState.ROUND_ACTIVE,
-    roundScores: [],
-    actionToUserId: 1,
-    cardsOnTopOfDiscardPile: [],
-  };
-}
 
 describe("YanivGameShowComponent", () => {
   let component: YanivGameShowComponent;
@@ -34,97 +20,5 @@ describe("YanivGameShowComponent", () => {
 
   it("should create", () => {
     expect(component).toBeTruthy();
-  });
-
-  describe("getUniquePlayerShortName", () => {
-    it("returns first letter if unique", () => {
-      // arrange
-      const playerStates = [
-        { userId: 1, username: "Adam", numberOfCards: 0, cards: [] },
-        { userId: 2, username: "John", numberOfCards: 0, cards: [] },
-      ];
-      component.game = buildTestGame(playerStates);
-
-      // act
-      const result = component.getUniquePlayerShortName(playerStates[0]);
-
-      // assert
-      expect(result).toEqual("A");
-    });
-
-    it("returns first two letter if unique", () => {
-      // arrange
-      const playerStates = [
-        { userId: 1, username: "John", numberOfCards: 0, cards: [] },
-        { userId: 2, username: "Jane", numberOfCards: 0, cards: [] },
-      ];
-      component.game = buildTestGame(playerStates);
-
-      // act
-      const result = component.getUniquePlayerShortName(playerStates[0]);
-
-      // assert
-      expect(result).toEqual("Jo");
-    });
-
-    it("returns first three letter if unique", () => {
-      // arrange
-      const playerStates = [
-        { userId: 1, username: "Charlie", numberOfCards: 0, cards: [] },
-        { userId: 2, username: "Chuck", numberOfCards: 0, cards: [] },
-      ];
-      component.game = buildTestGame(playerStates);
-
-      // act
-      const result = component.getUniquePlayerShortName(playerStates[0]);
-
-      // assert
-      expect(result).toEqual("Cha");
-    });
-
-    it("returns first unique three letter if possible", () => {
-      // arrange
-      const playerStates = [
-        { userId: 1, username: "Chad", numberOfCards: 0, cards: [] },
-        { userId: 2, username: "Charlie", numberOfCards: 0, cards: [] },
-      ];
-      component.game = buildTestGame(playerStates);
-
-      // act
-      const result = component.getUniquePlayerShortName(playerStates[0]);
-
-      // assert
-      expect(result).toEqual("Chd");
-    });
-
-    it("returns first three letters if is subset of other name", () => {
-      // arrange
-      const playerStates = [
-        { userId: 1, username: "Char", numberOfCards: 0, cards: [] },
-        { userId: 2, username: "Charlie", numberOfCards: 0, cards: [] },
-      ];
-      component.game = buildTestGame(playerStates);
-
-      // act
-      const result = component.getUniquePlayerShortName(playerStates[0]);
-
-      // assert
-      expect(result).toEqual("Cha");
-    });
-
-    it("returns other three letter combo if name is superset of other name", () => {
-      // arrange
-      const playerStates = [
-        { userId: 1, username: "Char", numberOfCards: 0, cards: [] },
-        { userId: 2, username: "Charlie", numberOfCards: 0, cards: [] },
-      ];
-      component.game = buildTestGame(playerStates);
-
-      // act
-      const result = component.getUniquePlayerShortName(playerStates[1]);
-
-      // assert
-      expect(result).toEqual("Chl");
-    });
   });
 });
