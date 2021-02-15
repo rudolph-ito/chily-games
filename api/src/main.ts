1d5bbf0375e0b1acfe2cebce28ce084f0cde630d
Charlie Rudolph <charles.w.rudolph@gmail.com>
2021-02-15 15:48:59 -0800

    allow local testing on home network, remove unneeded cors support (#167)

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,6 @@
 import { startServer } from "./controllers";
-import { valueOrDefault } from "./shared/utilities/value_checker";
 import { createClient } from "redis";
 
-const corsOrigins = valueOrDefault(process.env.CORS_ORIGINS, "").split(",");
 if (process.env.PORT == null) {
   throw new Error("PORT environment variable in required");
 }
@@ -14,7 +12,6 @@ const sessionSecret = process.env.SESSION_SECRET;
 const redisUrl = process.env.REDIS_URL ?? "redis://localhost:6379";
 
 startServer({
-  corsOrigins,
   port,
   redisClientBuilder: () => createClient({ url: redisUrl }),
   sessionSecret,
