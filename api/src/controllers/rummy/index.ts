38793f9f7ccad95105f17f5fb97c745ab21a0c65
Charles Rudolph <charles.w.rudolph@gmail.com>
2022-05-30 17:03:23 -0700

    controller

---
+++ api/src/controllers/rummy/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import { getGameRouter } from "./game";
+import { RedisClient } from "redis";
+
+export function getRummyRouter(
+  authenticationRequired: express.Handler,
+  publishRedisClient: RedisClient
+): express.Router {
+  const router = express.Router();
+  router.use(
+    "/games",
+    getGameRouter(authenticationRequired, publishRedisClient)
+  );
+  return router;
+}
