6f29b84680fec4059dd52d1f997dbcc6a52069e2
Charles Rudolph <1676758+charlierudolph@users.noreply.github.com>
2025-03-15 09:32:23 -0700

    frontend tests passing

--- frontend/src/app/components/rummikub/rummikub-game-show/rummikub-game-show.component.spec.ts
+++ frontend/src/app/components/rummikub/rummikub-game-show/rummikub-game-show.component.spec.ts
@@ -1,14 +1,37 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { RummikubGameShowComponent } from "./rummikub-game-show.component";
+import { AppModule } from "src/app/app.module";
+import { RummikubGameService } from "src/app/services/rummikub/rummikub-game-service";
+import { UserService } from "src/app/services/user.service";
+import { ChatService } from "src/app/services/chat.service";
+import { of } from "rxjs";
+import { getMockGame } from "src/app/test/rummikub/mock-data";
 
 describe("RummikubGameShowComponent", () => {
   let component: RummikubGameShowComponent;
   let fixture: ComponentFixture<RummikubGameShowComponent>;
+  let mockGameService: Partial<RummikubGameService>;
+  let mockUserService: Partial<UserService>;
+  let mockChatService: Partial<ChatService>;
 
   beforeEach(() => {
+    mockGameService = {
+      get: () => of(getMockGame()),
+    };
+    mockUserService = {
+      get: () => of({ userId: 1, username: "test", displayName: "test" }),
+    };
+    mockChatService = {
+      get: () => of({ chatMessages: [] }),
+    };
     TestBed.configureTestingModule({
-      declarations: [RummikubGameShowComponent],
+      imports: [AppModule],
+      providers: [
+        { provide: RummikubGameService, useValue: mockGameService },
+        { provide: UserService, useValue: mockUserService },
+        { provide: ChatService, useValue: mockChatService },
+      ],
     });
     fixture = TestBed.createComponent(RummikubGameShowComponent);
     component = fixture.componentInstance;
