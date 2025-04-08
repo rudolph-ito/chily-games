4d0b2ffafce6cf77f19aea845f93fb66a5312933
Charles Rudolph <1676758+charlierudolph@users.noreply.github.com>
2025-04-07 20:05:26 -0700

    fix

--- frontend/src/app/components/rummikub/rummikub-new-game-dialog/rummikub-new-game-dialog.component.spec.ts
+++ frontend/src/app/components/rummikub/rummikub-new-game-dialog/rummikub-new-game-dialog.component.spec.ts
@@ -1,16 +1,28 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { RummikubNewGameDialogComponent } from "./rummikub-new-game-dialog.component";
+import { AppModule } from "src/app/app.module";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
 
 describe("RummikubNewGameDialogComponent", () => {
   let component: RummikubNewGameDialogComponent;
   let fixture: ComponentFixture<RummikubNewGameDialogComponent>;
+  let matDialogRef: Partial<MatDialogRef<RummikubNewGameDialogComponent>>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [RummikubNewGameDialogComponent],
-    }).compileComponents();
-
+  beforeEach(() => {
+    matDialogRef = {
+      close: jest.fn(),
+    };
+    TestBed.configureTestingModule({
+      imports: [AppModule],
+      providers: [
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {},
+        },
+        { provide: MatDialogRef, useValue: matDialogRef },
+      ],
+    });
     fixture = TestBed.createComponent(RummikubNewGameDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
